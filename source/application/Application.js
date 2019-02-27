7df5e1a3b6875b7ce2a9c8d616997be1d8efa777
Sergio <33621369+Sergio-Ka@users.noreply.github.com>
2019-02-27 17:36:36 +0700

    delete Application.js

--- source/application/Application.js
+++
@@ -1,7 +0,0 @@
-import FacadeAndPublisherOfModel from './model-facade';
-import View from './view';
-import Controller from './controller';
-
-const model = new FacadeAndPublisherOfModel();
-const view = new View();
-const controller = new Controller(model, view);
